3f307c38dc880d78a8409baa7b266cbf594c0480
Domen Lisjak <domen.lisjak@gmail.com>
2018-12-02 20:36:00 +0100

    Updates

---
+++ precache-manifest.238403bc3978dcc1fbcfbdd440273413.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "a5144e54e49101fc74fb",
+    "url": "/portfolio/static/js/main.a5144e54.chunk.js"
+  },
+  {
+    "revision": "1a1b8469f27c15517f3c",
+    "url": "/portfolio/static/js/1.1a1b8469.chunk.js"
+  },
+  {
+    "revision": "cb77d69c2f6e6f692d78fa0a5c656ed7",
+    "url": "/portfolio/index.html"
+  }
+];
